e5849e515c581864c01ca6c597394c4d168180f3
Yadi Wang <wangydtju@gmail.com>
2022-04-30 22:41:47 +0000

    initial commit

---
+++ server.py
@@ -0,0 +1,35 @@
+"""Server for movie ratings app."""
+import os
+
+from flask import (Flask, render_template, request, flash, session, redirect, make_response)
+
+from model import connect_to_db, db
+
+import crud
+
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = os.environ["SECRET_KEY"]
+
+app.jinja_env.undefined = StrictUndefined
+
+
+# Replace this with routes and view functions!
+@app.route('/')
+def index():
+    """go to homepage"""
+    return render_template("homepage.html")
+
+@app.route('/movies')
+def get_movies():
+    """go to movies"""
+    movies = crud.all_movies()
+    return render_template("all_movies.html", movies = movies)
+
+
+
+if __name__ == "__main__":
+    # DebugToolbarExtension(app)
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
